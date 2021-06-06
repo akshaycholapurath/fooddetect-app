2994d8adc58afedba0a304dbd94b939a074abb0e
Akshay <akshayc1991@gmail.com>
2021-06-06 14:10:56 +0530

    Fetch error fixed

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ document.getElementById("button1").addEventListener("click", function(){
         let value = document.getElementById("uri").value
         document.getElementById("image").src = value;
         const div = document.getElementById("pred")
-        fetch('http://127.0.0.1:5000/prediction',{
+        fetch('https://fooddetect-app.herokuapp.com/prediction',{
               method:'post',
               headers:{'Content-Type':'application/json'},
               body:JSON.stringify({
